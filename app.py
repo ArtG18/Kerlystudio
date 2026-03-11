bbacd8d797d616bca5c7f04acfb0463f814962c1
ArtG18 <manosalvagar@gmail.com>
2026-03-11 17:34:28 -0300

    fix database init

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
+init_db()
 
 # ---------------- DATABASE ----------------
 
